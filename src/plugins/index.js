0773953a2c72ae87da031263884ed293bda72661
guilhermecarneiroo <guilhermecarneiroo@gmail.com>
2018-10-04 15:58:25 -0300

    remove jsErrors files

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,7 +1,5 @@
 const prometheusPlugin = require('./prometheus');
-const newRelicPlugin = require('./newrelic');
 
 module.exports = {
-  prometheusPlugin,
-  newRelicPlugin
+  prometheusPlugin
 };
--- src/plugins/newrelic/index.js
+++
@@ -1,9 +0,0 @@
-const scheduler = require('../../utils/scheduler');
-const jsErrors = require('./javascriptErrors');
-const start = (APP_ID, API_KEY) => {
-  jsErrors.start();
-  scheduler.start(jsErrors.scheduleTask(APP_ID, API_KEY));
-}
-module.exports = {
-  start
-};
