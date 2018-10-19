d8f4134c4ac08a2d8429acda8ca482c0ebd026ee
Diogo Nicoleti <diogo.nicoleti@gmail.com>
2018-10-19 16:46:17 -0300

    Add double quotes to vars

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -7,9 +7,9 @@ echo "Logging to registry"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "Building docker image"
-docker build -t $DOCKER_IMAGE:latest .
-docker tag $DOCKER_IMAGE:latest $DOCKER_IMAGE:$TRAVIS_TAG
+docker build -t "$DOCKER_IMAGE":latest .
+docker tag "$DOCKER_IMAGE":latest "$DOCKER_IMAGE":"$TRAVIS_TAG"
 
 echo "Pushing docker image"
-docker push $DOCKER_IMAGE:latest
-docker push $DOCKER_IMAGE:$TRAVIS_TAG
+docker push "$DOCKER_IMAGE":latest
+docker push "$DOCKER_IMAGE":"$TRAVIS_TAG"
